4164c75bd20636920bf7be44e47a971a705f0ddf
WhatMeWorry <WhatMeWorry@users.noreply.github.com>
2024-04-10 18:12:05 -0500

    got embedded structs to work
    got embedded structs to work

--- source/app.d
+++ source/app.d
@@ -1,6 +1,9 @@
+
+
 /// Simple example of a GLFW application that creates a black window and reads some input and output
 ///
 /// Does not load OpenGL / Vulkan.
+
 module app;
 
 import glfw3.api;
@@ -10,78 +13,66 @@ import std.stdio : writeln;
 import bindbc.sdl;
 import bindbc.loader;
 
-//import loader = bindbc.loader.sharedlib;
-//import bindbc.loader.sharedlib;
 
-
-bool loadLib()
+bool loadSimpleDirectMediaLibary()
 {
-    writeln("Inside LoadLib");
-    //LoadMsg ret = loadSDL();
     SDLSupport ret = loadSDL();	
 	
-    writeln("ret = ", ret);
-	
-    if(ret != sdlSupport)
+    if(ret == sdlSupport)
     {
-        writeln("ret does not say sdlSupport");
-        //Log the error info
-		//foreach(info; loader.errors)
-        //{
-            // A hypothetical logging function. Note that `info.error` and
-            // `info.message` are null-terminated `const(char)*`, not `string`.
-            //logError(info.error, info.message);
-        //}
+        SDL_version ver;
+		SDL_GetVersion(&ver);
 		
-		//Optionally construct a user-friendly error message for the user
-		string msg;
-		if(ret == SDLSupport.noLibrary)
-		{
-		    writeln("ret says noLibrary");
-            //msg = "This application requires the SDL library.";
-        }
-        else
-        {
-			//SDL_version version;
-			//SDL_GetVersion(&version);
-			//msg = "Your SDL version is too low: "~ itoa(version.major)~"."~
-			//	itoa(version.minor)~"."~
-			//	itoa(version.patch)~
-			//	". Please upgrade to 2.0.16+.";
-		}
-		//A hypothetical message box function
-		//showMessageBox(msg);
-		return false;
-	}
-	writeln("ret says sdlSupport");
-	return true;
+        writeln("version = ", ver);	  // displays: version = SDL_version(2, 30, 2)
+        return true;
+    }
+
+    writeln("SDL library was not found or bad version");
+    return false;
 }
 
+
+Globals g;
+
+/// Global variables
+/// Idea taken from Mike Shah
+
+struct GLFW_STRUCT
+{	
+    const int SCREEN_WIDTH = 640;
+    const int SCREEN_HEIGHT = 480;		
+    GLFWwindow* window;  
+}
+
+struct SDL_STRUCT
+{
+    const int SCREEN_WIDTH = 640;
+    const int SCREEN_HEIGHT = 480;		
+    SDL_Window* window = null;            // The window we'll be rendering to
+    SDL_Surface* screenSurface = null;    //The surface contained by the window
+}	
+
+struct Globals
+{
+    int i;
+    GLFW_STRUCT glfw;
+    SDL_STRUCT sdl;
+}	
+ 
 
 
 
 
 int main() 
 {
-    GLFWwindow* window;
     glfwSetErrorCallback(&errorCallback);
 
-	if (!glfwInit()) { return -1; }
-	scope(exit) glfwTerminate();
+    if (!glfwInit()) { return -1; }
+    scope(exit) glfwTerminate();
 	
-	loadLib();
-	
-	
-	//Screen dimension constants
-    const int SCREEN_WIDTH = 640;
-    const int SCREEN_HEIGHT = 480;
-    //The window we'll be rendering to
-    SDL_Window* sdlWindow = null;
-    
-    //The surface contained by the window
-    SDL_Surface* screenSurface = null;
+    loadSimpleDirectMediaLibary();
 
-    //Initialize SDL
+    // Initialize SDL
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
     {
         writeln( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
@@ -89,190 +80,205 @@ int main()
     else
     {
         //Create window
-        sdlWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( sdlWindow == null )
+        g.sdl.window = SDL_CreateWindow( "SDL Tutorial", 
+                                         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+                                         g.sdl.SCREEN_WIDTH, g.sdl.SCREEN_HEIGHT, 
+                                         SDL_WINDOW_SHOWN );
+        if( g.sdl.window == null )
         {
-            printf( "sdlWindow could not be created! SDL_Error: %s\n", SDL_GetError() );
+            printf( "sdl Window could not be created! SDL_Error: %s\n", SDL_GetError() );
         }	
         else
         {
             //Get window surface
-            screenSurface = SDL_GetWindowSurface( sdlWindow );
+            g.sdl.screenSurface = SDL_GetWindowSurface( g.sdl.window );
 
             //Fill the surface white
-            SDL_FillRect( screenSurface, null, SDL_MapRGB( screenSurface.format, 0xFF, 0xFF, 0xFF ) );
+            SDL_FillRect( g.sdl.screenSurface, null, SDL_MapRGB( g.sdl.screenSurface.format, 0xFF, 0xFF, 0xFF ) );
             
             //Update the surface
-            SDL_UpdateWindowSurface( sdlWindow );
+            SDL_UpdateWindowSurface( g.sdl.window );
 
             //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+            SDL_Event e; bool quit = false; 
+            while( quit == false )
+            { 
+                while( SDL_PollEvent(&e) )
+                { 
+                    if( e.type == SDL_QUIT ) quit = true; 
+                } 
+            }
         }
     }  
 	
 	
 	
-	
-    //SDLSupport ret = loadSDL();	
-    //writeln("ret = ", ret);
-	
-	//SDL_Init(SDL_INIT_VIDEO);
 
-	printClipboardState();
-	//printJoystickState();
-	//printMonitorState();
+    //printClipboardState();
+    //printJoystickState();
+    //printMonitorState();
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-	WindowData data;
-	window = glfwCreateWindow(800, 600, "Black window - press F11 to toggle fullscreen, press ESC to exit", null, null);
-	scope(exit) glfwDestroyWindow(window);
-	if (!window) 
-	{ 
+    WindowData data;
+    g.glfw.window = glfwCreateWindow(800, 600, "Black window - press F11 to toggle fullscreen, press ESC to exit", null, null);
+    scope(exit) glfwDestroyWindow(g.glfw.window);
+    if (!g.glfw.window) 
+    { 
         glfwTerminate();
-		return -1;
-	}
-	glfwSetWindowUserPointer(window, &data);
+        return -1;
+    }
+    glfwSetWindowUserPointer(g.glfw.window, &data);
 
-	glfwSetKeyCallback(window, &keyCallback);
-	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1); // Set vsync on so glfwSwapBuffers will wait for monitor updates.
-	// note: 1 is not a boolean! Set e.g. to 2 to run at half the monitor refresh rate.
+    glfwSetKeyCallback(g.glfw.window, &keyCallback);
+    glfwMakeContextCurrent(g.glfw.window);
+    glfwSwapInterval(1); // Set vsync on so glfwSwapBuffers will wait for monitor updates.
+    // note: 1 is not a boolean! Set e.g. to 2 to run at half the monitor refresh rate.
 
-	double oldTime = glfwGetTime();
-	while (!glfwWindowShouldClose(window)) 
-	{
-		const newTime = glfwGetTime();
-		const elapsedTime = newTime - oldTime;
-		oldTime = newTime;
+    double oldTime = glfwGetTime();
+    while (!glfwWindowShouldClose(g.glfw.window)) 
+    {
+        const newTime = glfwGetTime();
+        const elapsedTime = newTime - oldTime;
+        oldTime = newTime;
 
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-	return 0;
+        glfwSwapBuffers(g.glfw.window);
+        glfwPollEvents();
+    }
+    return 0;
 }
 
 /// Data stored in the window's user pointer
 ///
 /// Note: assuming you only have one window, you could make these global variables.
+
 struct WindowData 
 {
-	// These are stored in the window's user data so that when exiting fullscreen,
-	// the window can be set to the position where it was before entering fullscreen
-	// instead of resetting to e.g. (0, 0)
-	int xpos;
-	int ypos;
-	int width;
-	int height;
+    // These are stored in the window's user data so that when exiting fullscreen,
+    // the window can be set to the position where it was before entering fullscreen
+    // instead of resetting to e.g. (0, 0)
+    int xpos;
+    int ypos;
+    int width;
+    int height;
 
-	@nogc nothrow void update(GLFWwindow* window) 
-	{
-		glfwGetWindowPos(window, &this.xpos, &this.ypos);
-		glfwGetWindowSize(window, &this.width, &this.height);
-	}
+    @nogc nothrow void update(GLFWwindow* window) 
+    {
+        glfwGetWindowPos(window, &this.xpos, &this.ypos);
+        glfwGetWindowSize(window, &this.width, &this.height);
+    }
 }
+
 
 extern(C) @nogc nothrow void errorCallback(int error, const(char)* description) 
 {
-	fprintf(stderr, "Error: %s\n", description);
+    fprintf(stderr, "Error: %s\n", description);
 }
+
 
 extern(C) @nogc nothrow void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) 
 {
-	if (action == GLFW_PRESS) 
-	{
-		switch (key) 
-		{
-			case GLFW_KEY_ESCAPE:
-				glfwSetWindowShouldClose(window, GLFW_TRUE);
-				break;
-			case GLFW_KEY_F11:
-				toggleFullScreen(window);
-				break;
-			default: break;
-		}
-	}
+    if (action == GLFW_PRESS) 
+    {
+        switch (key) 
+        {
+            case GLFW_KEY_ESCAPE:
+                glfwSetWindowShouldClose(window, GLFW_TRUE);
+                break;
+            case GLFW_KEY_F11:
+                toggleFullScreen(window);
+                break;
+            default: break;
+        }
+    }
 }
+
 
 @nogc nothrow void toggleFullScreen(GLFWwindow* window) 
 {
-	WindowData* wd = cast(WindowData*) glfwGetWindowUserPointer(window);
-	assert(wd);
-	if (glfwGetWindowMonitor(window)) 
-	{
-		glfwSetWindowMonitor(window, null, wd.xpos, wd.ypos, wd.width, wd.height, 0);
-	} 
-	else 
-	{
-		GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-		if (monitor) 
-		{
-			const GLFWvidmode* mode = glfwGetVideoMode(monitor);
-			wd.update(window);
-			glfwSetWindowMonitor(window, monitor, 0, 0, mode.width, mode.height, mode.refreshRate);
-		}
-	}
+    WindowData* wd = cast(WindowData*) glfwGetWindowUserPointer(window);
+    assert(wd);
+    if (glfwGetWindowMonitor(window)) 
+    {
+        glfwSetWindowMonitor(window, null, wd.xpos, wd.ypos, wd.width, wd.height, 0);
+    } 
+    else 
+    {
+        GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+        if (monitor) 
+        {
+            const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+            wd.update(window);
+            glfwSetWindowMonitor(window, monitor, 0, 0, mode.width, mode.height, mode.refreshRate);
+        }
+    }
 }
 
 
-void printClipboardState() {
-	printf("Clipboard contents: `%s.80`\n", glfwGetClipboardString(null));
+
+void printClipboardState() 
+{
+    printf("Clipboard contents: `%s.80`\n", glfwGetClipboardString(null));
 }
 
 
 void printMonitorState() 
 {
-	int monitorsLength;
-	GLFWmonitor** monitorsPtr = glfwGetMonitors(&monitorsLength);
-	GLFWmonitor*[] monitors = monitorsPtr[0..monitorsLength];
+    int monitorsLength;
+    GLFWmonitor** monitorsPtr = glfwGetMonitors(&monitorsLength);
+    GLFWmonitor*[] monitors = monitorsPtr[0..monitorsLength];
 
-	foreach(GLFWmonitor* mt; monitors) 
-	{
-		int widthMM, heightMM;
-		int xpos, ypos, width, height;
-		glfwGetMonitorPos(mt, &xpos, &ypos);
-		glfwGetMonitorPhysicalSize(mt, &widthMM, &heightMM);
-		const(GLFWvidmode)* mode = glfwGetVideoMode(mt);
-		printf("Monitor `%s` has size %dx%d mm\n", glfwGetMonitorName(mt), widthMM, heightMM);
-		printf("  current video mode: %dx%d %dHz r%dg%db%d\n", mode.width, mode.height, mode.refreshRate, mode.redBits, mode.greenBits, mode.blueBits);
-		printf("  position: %d, %d\n", xpos, ypos);
-		glfwGetMonitorWorkarea(mt, &xpos, &ypos, &width, &height);
-		printf("  work area: (%d, %d), size (%d, %d)\n", xpos, ypos, width, height);
-	}
+    foreach(GLFWmonitor* mt; monitors) 
+    {
+        int widthMM, heightMM;
+        int xpos, ypos, width, height;
+        glfwGetMonitorPos(mt, &xpos, &ypos);
+        glfwGetMonitorPhysicalSize(mt, &widthMM, &heightMM);
+        const(GLFWvidmode)* mode = glfwGetVideoMode(mt);
+        printf("Monitor `%s` has size %dx%d mm\n", glfwGetMonitorName(mt), widthMM, heightMM);
+        printf("  current video mode: %dx%d %dHz r%dg%db%d\n", mode.width, mode.height, mode.refreshRate, mode.redBits, mode.greenBits, mode.blueBits);
+        printf("  position: %d, %d\n", xpos, ypos);
+        glfwGetMonitorWorkarea(mt, &xpos, &ypos, &width, &height);
+        printf("  work area: (%d, %d), size (%d, %d)\n", xpos, ypos, width, height);
+    }
 }
+
 
 void printJoystickState() 
 {
-	for (int js = GLFW_JOYSTICK_1; js <= GLFW_JOYSTICK_LAST; js++) 
-	{
-		if (glfwJoystickPresent(js)) 
-		{
-			//glfwSetJoystickRumble(js, /*slow*/ 0.25, /*fast*/ 0.25);
-			printf("Joystick %d has name `%s` and GUID `%s`\n", js, glfwGetJoystickName(js), glfwGetJoystickGUID(js));
-			int buttonsLength, axesLength, hatsLength;
-			const(ubyte)* buttonsPtr = glfwGetJoystickButtons(js, &buttonsLength);
-			const(float)* axesPtr = glfwGetJoystickAxes(js, &axesLength);
-			const(ubyte)* hatsPtr = glfwGetJoystickHats(js, &hatsLength);
-			const(ubyte)[] buttons = buttonsPtr[0..buttonsLength];
-			const(float)[] axes = axesPtr[0..axesLength];
-			const(ubyte)[] hats = hatsPtr[0..hatsLength];
-			printf("number of axes: %d\n", cast(int) axes.length);
+    for (int js = GLFW_JOYSTICK_1; js <= GLFW_JOYSTICK_LAST; js++) 
+    {
+        if (glfwJoystickPresent(js)) 
+        {
+            //glfwSetJoystickRumble(js, /*slow*/ 0.25, /*fast*/ 0.25);
+            printf("Joystick %d has name `%s` and GUID `%s`\n", js, glfwGetJoystickName(js), glfwGetJoystickGUID(js));
+            int buttonsLength, axesLength, hatsLength;
+            const(ubyte)* buttonsPtr = glfwGetJoystickButtons(js, &buttonsLength);
+            const(float)* axesPtr = glfwGetJoystickAxes(js, &axesLength);
+            const(ubyte)* hatsPtr = glfwGetJoystickHats(js, &hatsLength);
+            const(ubyte)[] buttons = buttonsPtr[0..buttonsLength];
+            const(float)[] axes = axesPtr[0..axesLength];
+            const(ubyte)[] hats = hatsPtr[0..hatsLength];
+            printf("number of axes: %d\n", cast(int) axes.length);
 
-			if (glfwJoystickIsGamepad(js)) 
-			{
-				printf("  It is a gamepad with name `%s`\n", glfwGetGamepadName(js));
-				GLFWgamepadstate state;
-				if (glfwGetGamepadState(js, &state)) 
-				{
-					printf("Left stick: %f,%f\n", state.axes[GLFW_GAMEPAD_AXIS_LEFT_X], state.axes[GLFW_GAMEPAD_AXIS_LEFT_Y]);
-					printf("A: %d, B: %d\n", state.buttons[GLFW_GAMEPAD_BUTTON_A], state.buttons[GLFW_GAMEPAD_BUTTON_B]);
-				}
-			}
-		} 
-		else 
-		{
-			printf("Joystick %d not present\n", js);
-		}
-	}
+            if (glfwJoystickIsGamepad(js)) 
+            {
+                printf("  It is a gamepad with name `%s`\n", glfwGetGamepadName(js));
+                GLFWgamepadstate state;
+                if (glfwGetGamepadState(js, &state)) 
+                {
+                    printf("Left stick: %f,%f\n", state.axes[GLFW_GAMEPAD_AXIS_LEFT_X], state.axes[GLFW_GAMEPAD_AXIS_LEFT_Y]);
+                    printf("A: %d, B: %d\n", state.buttons[GLFW_GAMEPAD_BUTTON_A], state.buttons[GLFW_GAMEPAD_BUTTON_B]);
+                }
+            }
+        } 
+        else 
+        {
+            printf("Joystick %d not present\n", js);
+        }
+    }
+	
+	
+
 }
