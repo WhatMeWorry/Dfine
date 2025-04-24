2e61501a415f241e4a60caf0291bc5c7b90b8606
WhatMeWorry <WhatMeWorry@users.noreply.github.com>
2025-04-24 16:57:05 -0500

    improvements
    improvements

--- breakup.d
+++ breakup.d
@@ -9,6 +9,7 @@ import std.range : empty;  // for aa
 import core.stdc.stdlib : exit;
 import datatypes : Location;
 import a_star.spot : writeAndPause;
+import core.stdc.stdio : printf;
 
 import std.string : toStringz;  // converts D string to C string
 import std.conv : to;           // to!string(c_string)  converts C string to D string 
@@ -64,25 +65,61 @@ SDL_Texture* LoadImageToTexture(SDL_Renderer *renderer, string fileName)
     return texture;
 }
 
+void textureProperties(SDL_Texture *texture)
+{
+    uint format;
+    string pixelFormat;
+    int w; int h;
     
+    SDL_QueryTexture(texture, &format, null, &w, &h);
+    writeln("texture width, height = ", w, " X ", h);
+
+    pixelFormat = to!string(SDL_GetPixelFormatName(format));
+    writeln("main pixelFormat = ", pixelFormat);
+}
+
+D2 getTextureSize(SDL_Texture *texture)
+{
+    uint format;
+    D2 dims;
+    
+    SDL_QueryTexture(texture, &format, null, &dims.w, &dims.h);
+    return dims;
+}
+
 void breakup1()
 {
-    Graphic mini;
+    Graphic m1;
+    Graphic m2;
     Graphic main;
 
-    mini.win.h = 1000;
-    mini.win.w = cast (int)(cast(double) mini.win.h * 0.6294);
+    m1.win.h = 1000;
+    m1.win.w = cast (int)(cast(double) m1.win.h * 0.6294);
 
+    m1.window = SDL_CreateWindow("IGNORE", 
+                                 SDL_WINDOWPOS_CENTERED, 
+                                 SDL_WINDOWPOS_CENTERED,
+                                 m1.win.w, 
+                                 m1.win.h, 
+                                 SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
 
-    mini.window = SDL_CreateWindow("SDL2 PNG Viewer", 
-                                   SDL_WINDOWPOS_CENTERED, 
-                                   SDL_WINDOWPOS_CENTERED,
-                                   mini.win.w, 
-                                   mini.win.h, 
-                                   SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-    
-    mini.renderer = SDL_CreateRenderer(mini.window, -1, SDL_RENDERER_ACCELERATED);
-     
+    m1.renderer = SDL_CreateRenderer(m1.window, -1, SDL_RENDERER_ACCELERATED);
+
+    m1.texture = LoadImageToTexture(m1.renderer, "./images/1.png");  // file to texture
+
+    m2 = m1;
+
+    m2.window = SDL_CreateWindow("IGNORE", 
+                                 SDL_WINDOWPOS_CENTERED, 
+                                 SDL_WINDOWPOS_CENTERED,
+                                 m2.win.w, 
+                                 m2.win.h, 
+                                 SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+
+    m2.renderer = SDL_CreateRenderer(m2.window, -1, SDL_RENDERER_ACCELERATED);
+
+    m2.texture = LoadImageToTexture(m2.renderer, "./images/2.png");  // file to texture
+
     main.win.w = 1000;
     main.win.h = 1000;
 
@@ -95,22 +132,52 @@ void breakup1()
 
     main.renderer = SDL_CreateRenderer(main.window, -1, SDL_RENDERER_ACCELERATED);
 
-    mini.surface = LoadImageToSurface("./images/1.png");
-
-    mini.texture = LoadImageToTexture(mini.renderer, "./images/1.png");
+    main.texture = LoadImageToTexture(main.renderer, "./images/2.png");
     
-    main.texture = LoadImageToTexture(main.renderer, "./images/1.png");
+/+
+    main.texture = SDL_CreateTexture(main.renderer,
+                                     SDL_PIXELFORMAT_ARGB8888,
+                                     SDL_TEXTUREACCESS_STATIC, 
+                                     main.win.w, main.win.h); +/
 
-    uint format;
-    SDL_QueryTexture(mini.texture, &format, null, &mini.tex.w, &mini.tex.h);
-    writeln("mini.tex.w X mini.tex.h = ", mini.tex.w, " X ", mini.tex.h);
+    //SDL_SetRenderTarget(main.renderer, largeTexture);  // specify that you want to draw to the target texture instead of the screen.
+
+    textureProperties(m1.texture);
+    textureProperties(m2.texture);
+    textureProperties(main.texture);
     
-    string pixelFormat = to!string(SDL_GetPixelFormatName(format));
-    writeln("pixelFormat = ", pixelFormat);
+    //D2 d2 = getTextureSize(m1.texture);
+    //int w = d2.w * 2;
+    //int h = d2.h * 2;
+    //writeln("w x h = ", w, " ", h);
 
-    mini.sur.w = mini.surface.w;
-    mini.sur.h = mini.surface.h;    
-    writeln("mini.sur.(w,h) = ", mini.sur.w, " X ", mini.sur.h);
+    
+    //SDL_Surface* hugeSurface = SDL_CreateRGBSurface(0, w, h, 32, 0, 0, 0, 0);
+
+    //SDL_SetRenderDrawColor(main.renderer, 0, 0, 0xFF, SDL_ALPHA_OPAQUE);
+    //SDL_RenderClear(main.renderer);
+
+
+    SDL_Rect destRect1 = {0, 0, 500, 500 /+main.win.w/2, main.win.h+/}; // Example destination rectangle
+    
+    writeln("destRect1 = ", destRect1);
+    
+    SDL_RenderCopy(m1.renderer, m1.texture, null, null);
+
+    SDL_Rect destRect2 = {main.win.w/2, 0, main.win.w, main.win.h}; // Example destination rectangle
+    
+    //writeln("destRec2 = ", destRect2);
+    
+    //SDL_RenderCopy(main.renderer, m2.texture, null, &destRect2);
+
+    //SDL_RenderPresent(main.renderer);
+
+    SDL_RenderPresent(main.renderer);
+    SDL_RenderPresent(m1.renderer);
+    
+    //SDL_SetRenderTarget(main.renderer, null); // set the render target back to the screen 
+    
+    //SDL_RenderCopy(main.renderer, largeTexture, null, null);
 
     //writeAndPause("breakup.d line 84");
 
@@ -123,34 +190,6 @@ void breakup1()
                 running = false;
             }
         }
-
-        // Clear screen
-        SDL_RenderClear(mini.renderer);
-        SDL_RenderClear(main.renderer);
-        
-        SDL_Rect totalRect = { 0, 0, mini.sur.w, mini.sur.h };  // 6,668 x 10,594
-
-        SDL_Rect srcRect = { mini.tex.w/2, mini.tex.h/2, 500, 500 };
-        
-        //SDL_Rect destRect = { 0, imgHeight/2, mainWinWidth, mainWinHeight };
-
-        // Render image centered
-        SDL_Rect miniDestRect = { 0, 0, mini.sur.w, mini.sur.h };
-        
-        //SDL_Rect mainDestRect = { 0, 0, mainWinWidth, mainWinHeight };
-
-        SDL_RenderCopy(mini.renderer, mini.texture, /+&srcRect+/ null /+&totalRect+/, null /+&miniDestRect+/);
-        
-        // You need a renderer to create an SDL_Texture
-        
-        SDL_RenderCopy(main.renderer, main.texture, null, null );  // &srcRect /+&totalRect+/, &srcRect /+&miniDestRect+/);
-                                                             // SDL_PIXELFORMAT_ARGB8888
-        SDL_Texture* largeTexture = SDL_CreateTexture(main.renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, 100000, 100000);
-        
-        // SDL_Texture * SDL_CreateTextureFromSurface(SDL_Renderer * renderer, SDL_Surface * surface);
-        
-        
-        //SDL_RenderCopy(mRenderer, texture, /+&srcRect+/ null /+&totalRect+/, &miniDestRect);
         
         /+
         In SDL, you can't directly "create a surface from a texture" in the way you might imagine. 
@@ -171,8 +210,7 @@ void breakup1()
         +/
 
         // Update screen
-        SDL_RenderPresent(mini.renderer);
-        SDL_RenderPresent(main.renderer);
+        
     }
 }
 
@@ -222,3 +260,78 @@ SDL_RenderCopy(renderer, target_texture, NULL, NULL);
 // ... (SDL cleanup) ...
 
 +/
+
+/+
+  window = SDL_CreateWindow("Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+
+
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
+
+  // Create a texture (must be of type SDL_TEXTUREACCESS_TARGET for rendering)
+  texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 320, 240);
+
+  // 1. Render to the texture
+  SDL_SetRenderTarget(renderer, texture); // Set texture as target
+  SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF); // Red
+  SDL_RenderClear(renderer); // Clear the texture
+  SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0xFF); // Blue
+  SDL_RenderDrawLine(renderer, 0, 0, 320, 240);  // Draw a diagonal line
+  SDL_RenderSetViewport(renderer, NULL);
+
+  // 2. Reset target back to the window
+  SDL_SetRenderTarget(renderer, NULL); // Set window as target
+
+  // 3. Render the texture to the window
+  SDL_RenderClear(renderer); // Clear the window
+  SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0xFF);  // Green
+  SDL_Rect dstrect = { 100, 100, 320, 240 }; // Destination rectangle
+  SDL_RenderCopy(renderer, texture, NULL, &dstrect); // Copy the texture to the window
+
+  // Present the window
+  SDL_RenderPresent(renderer);
+
++/
+
+
+/+
+To copy pixels between SDL textures and surfaces, you can use 
+SDL_RenderReadPixels() to read from a texture into a surface
+SDL_UpdateTexture() to copy from a surface to a texture. 
+SDL_RenderCopy() to copy between textures if one of the textures is a render target. 
++/
+
+/+
+One option for displaying extremely large images is to split it up into multiple textures and render each. 
+
+
+To draw multiple SDL2 textures onto the same screen, you can render each texture individually within a 
+loop, then present the rendered image using SDL_RenderPresent. This approach allows you to position and 
+combine textures as needed. 
+
+
+while (running) 
+{
+    // Clear the renderer (optional, but good practice)
+    
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // Render texture1
+    SDL_Rect destRect1 = {x1, y1, width1, height1}; // Example destination rectangle
+    SDL_RenderCopy(renderer, texture1, NULL, &destRect1);
+
+    // Render texture2
+    SDL_Rect destRect2 = {x2, y2, width2, height2}; // Example destination rectangle
+    SDL_RenderCopy(renderer, texture2, NULL, &destRect2);
+
+    // ... render more textures if needed
+
+    // Present the rendered image
+    SDL_RenderPresent(renderer);
+}
+
++/
+
+
+
+
